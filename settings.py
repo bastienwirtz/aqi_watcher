2df8f69631b759adadd5c4846b645ad8b56030f8
Bastien Wirtz <bastien.wirtz@gmail.com>
2020-09-18 19:30:47 -0700

    Document settings

--- settings.py
+++ settings.py
@@ -8,6 +8,12 @@ INFLUXDB = {
 }
 
 POSTCODE = 94117
+
+# Get your airnow api key on https://docs.airnowapi.org
 AIRNOW_KEY = ""
+
+# Get your open weather map pi key on https://openweathermap.org/api
 OPENWEATHERMAP_KEY = ""
-PURPLEAIR_SENSORS = ""  # coma separated list
+
+# Get purple air sensor id by selecting a sensor on their map: https://www.purpleair.com/map, and looking into the Url for the `select=xxxx` parameter.
+PURPLEAIR_SENSORS = ""  # One or more sensors (coma separated list)
